b413f3f8efc20fed1d8276a999b363d0137b64b6
takanobuk <226429335+takanobuk@users.noreply.github.com>
2026-02-04 14:41:04 +0900

    プロジェクト ファイルを追加します。

---
+++ ListSerialPort/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ListSerialPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.ctlList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(16, 16);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 41);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "更新";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // ctlList
+            // 
+            this.ctlList.FormattingEnabled = true;
+            this.ctlList.HorizontalScrollbar = true;
+            this.ctlList.ItemHeight = 23;
+            this.ctlList.Location = new System.Drawing.Point(16, 72);
+            this.ctlList.Name = "ctlList";
+            this.ctlList.Size = new System.Drawing.Size(464, 234);
+            this.ctlList.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(496, 321);
+            this.Controls.Add(this.ctlList);
+            this.Controls.Add(this.btnUpdate);
+            this.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "シリアルポート一覧";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.ListBox ctlList;
+    }
+}
+
---
+++ ListSerialPort/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Management;
+using System.Linq;
+
+
+namespace ListSerialPort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateSerialPortList();
+        }
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateSerialPortList();
+        }
+
+
+        private void UpdateSerialPortList()
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+
+                ctlList.Items.Clear();
+
+                // WMIクエリでシリアルポート情報を取得
+                using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_SerialPort"))
+                {
+                    ManagementObjectCollection results = searcher.Get();
+
+                    // LINQを使ってCaption順に並べ替える
+                    var sortedResults = results.Cast<ManagementObject>()
+                                               .OrderBy(mo => mo["DeviceID"]);
+
+                    foreach (ManagementObject port in sortedResults)
+                    {
+                        string deviceId = port["DeviceID"]?.ToString() ?? "不明";
+                        string name = port["Name"]?.ToString() ?? "不明";
+                        string description = port["Description"]?.ToString() ?? "不明";
+
+                        //string portInfo = $"{deviceId} - {name} ({description})";
+                        string portInfo = $"{deviceId} - {description}";
+                        ctlList.Items.Add(portInfo);
+                    }
+                }
+
+                if (ctlList.Items.Count == 0)
+                {
+                    ctlList.Items.Add("シリアルポートが見つかりませんでした。");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"エラーが発生しました: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+    }
+}
